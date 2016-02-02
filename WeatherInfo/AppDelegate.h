b295dc8622cf67bf69b8aadd9e1f2f4d6e6d519b
Akib11 <Akibm92@gmail.com>
2016-02-02 23:02:02 +0000

    Initial Commit

---
+++ WeatherInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherInfo
+//
+//  Created by Akib Quraishi on 02/02/2016.
+//  Copyright © 2016 AkibiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherInfo
+//
+//  Created by Akib Quraishi on 02/02/2016.
+//  Copyright © 2016 AkibiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
